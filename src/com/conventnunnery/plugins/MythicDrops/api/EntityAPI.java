45a2ef68ba97a366165cf2ae0ad1280523447328
Richard Harrah <topplethenun@hawksharbor.info>
2013-03-21 17:57:15 -0400

    adding events

--- src/com/conventnunnery/plugins/MythicDrops/api/EntityAPI.java
+++ src/com/conventnunnery/plugins/MythicDrops/api/EntityAPI.java
@@ -1,7 +1,20 @@
+/*
+ * Copyright (c) 2013. ToppleTheNun
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.conventnunnery.plugins.MythicDrops.api;
 
 import com.conventnunnery.plugins.MythicDrops.MythicDrops;
+import com.conventnunnery.plugins.MythicDrops.events.CreatureEquippedWithItemStackEvent;
+import com.conventnunnery.plugins.MythicDrops.objects.CustomItem;
 import com.conventnunnery.plugins.MythicDrops.objects.Tier;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 
@@ -20,6 +33,40 @@ public class EntityAPI {
 		this.plugin = plugin;
 	}
 
+	public void equipEntity(LivingEntity entity, CustomItem customItem) {
+		float f;
+		if (customItem != null)
+			f = (float) customItem.getChance();
+		else
+			return;
+		ItemStack itemstack = customItem.toItemStack();
+		CreatureEquippedWithItemStackEvent cewise = new CreatureEquippedWithItemStackEvent(entity, itemstack);
+		Bukkit.getPluginManager().callEvent(cewise);
+		if (cewise.isCancelled())
+			return;
+		itemstack = cewise.getItemStack();
+		String itemType = getPlugin().getItemAPI().itemTypeFromMatData(itemstack.getData());
+		if (itemType != null && itemType.equalsIgnoreCase("boots")) {
+			entity.getEquipment().setBoots(itemstack);
+			entity.getEquipment().setBootsDropChance(f);
+		} else if (itemType != null && itemType
+				.equalsIgnoreCase("leggings")) {
+			entity.getEquipment().setLeggings(itemstack);
+			entity.getEquipment().setLeggingsDropChance(f);
+		} else if (itemType != null && itemType
+				.equalsIgnoreCase("chestplate")) {
+			entity.getEquipment().setChestplate(itemstack);
+			entity.getEquipment().setChestplateDropChance(f);
+		} else if (itemType != null && itemType
+				.equalsIgnoreCase("helmet")) {
+			entity.getEquipment().setHelmet(itemstack);
+			entity.getEquipment().setHelmetDropChance(f);
+		} else {
+			entity.getEquipment().setItemInHand(itemstack);
+			entity.getEquipment().setItemInHandDropChance(f);
+		}
+	}
+
 	/**
 	 * Equip entity.
 	 *
@@ -31,6 +78,11 @@ public class EntityAPI {
 		float f = 1.0F;
 		if (tier != null)
 			f = tier.getChanceToDrop();
+		CreatureEquippedWithItemStackEvent cewise = new CreatureEquippedWithItemStackEvent(entity, itemstack);
+		Bukkit.getPluginManager().callEvent(cewise);
+		if (cewise.isCancelled())
+			return;
+		itemstack = cewise.getItemStack();
 		String itemType = getPlugin().getItemAPI().itemTypeFromMatData(itemstack.getData());
 		if (itemType != null && itemType.equalsIgnoreCase("boots")) {
 			entity.getEquipment().setBoots(itemstack);
--- src/com/conventnunnery/plugins/MythicDrops/listeners/EntityListener.java
+++ src/com/conventnunnery/plugins/MythicDrops/listeners/EntityListener.java
@@ -1,7 +1,18 @@
+/*
+ * Copyright (c) 2013. ToppleTheNun
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.conventnunnery.plugins.MythicDrops.listeners;
 
 import com.conventnunnery.plugins.MythicDrops.MythicDrops;
 import com.conventnunnery.plugins.MythicDrops.api.DropAPI;
+import com.conventnunnery.plugins.MythicDrops.objects.CustomItem;
 import com.conventnunnery.plugins.MythicDrops.objects.Tier;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
@@ -64,13 +75,55 @@ public class EntityListener implements Listener {
 				&& (getPlugin().getRandom().nextDouble() < getPlugin()
 				.getPluginSettings().getPercentageCustomDrop())) {
 			if (!getPlugin().getDropAPI().getCustomItems().isEmpty()) {
-				getPlugin()
-						.getDropAPI()
-						.getCustomItems()
-						.get(getPlugin().getRandom().nextInt(getPlugin()
-								.getDropAPI().getCustomItems().size()))
-						.toItemStack();
-
+				double globalChanceToSpawn = getPlugin().getPluginSettings()
+						.getPercentageMobSpawnWithItemChance();
+				double mobChanceToSpawn = 0.0;
+				if (getPlugin().getPluginSettings()
+						.getAdvancedMobSpawnWithItemChanceMap()
+						.containsKey(event.getEntity().getType().name())) {
+					mobChanceToSpawn = getPlugin().getPluginSettings()
+							.getAdvancedMobSpawnWithItemChanceMap().get(event.getEntity().getType().name());
+				}
+				double chance = globalChanceToSpawn * mobChanceToSpawn;
+				for (int i = 0; i < 5; i++) {
+					if (getPlugin().getRandom().nextDouble() < chance) {
+						if (getPlugin().getPluginSettings().isAllowCustomToSpawn()
+								&& (getPlugin().getRandom().nextDouble() < getPlugin()
+								.getPluginSettings().getPercentageCustomDrop())) {
+							if (!getPlugin().getDropAPI().getCustomItems().isEmpty()) {
+								getPlugin()
+										.getEntityAPI()
+										.equipEntity(
+												event.getEntity(),
+												getPlugin()
+														.getDropAPI()
+														.getCustomItems()
+														.get(getPlugin().getRandom()
+																.nextInt(getPlugin()
+																		.getDropAPI()
+																		.getCustomItems()
+																		.size()))
+														.toItemStack(), null);
+								chance *= 0.5;
+							}
+							continue;
+						}
+						if (!getPlugin().getPluginSettings().isOnlyCustomItems() && !getPlugin()
+								.getDropAPI()
+								.getCustomItems().isEmpty()) {
+							CustomItem customItem = getPlugin()
+									.getDropAPI()
+									.getCustomItems()
+									.get(getPlugin().getRandom().nextInt(getPlugin()
+											.getDropAPI().getCustomItems().size()));
+							getPlugin().getEntityAPI().equipEntity(event.getEntity(),
+									customItem);
+							chance *= 0.5;
+						}
+						continue;
+					}
+					break;
+				}
 			}
 		}
 		if (event.getSpawnReason() == SpawnReason.SPAWNER
